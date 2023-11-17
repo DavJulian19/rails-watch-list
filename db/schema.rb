edcbe1b4426dab5c38a07c43a46480966da7ffe6
David Julian <david_a80@hotmail.com>
2023-11-17 11:42:34 +0100

    rails watch list-2

--- db/schema.rb
+++ db/schema.rb
@@ -30,14 +30,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_16_174542) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "movie_lists", force: :cascade do |t|
-    t.string "title"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_movie_lists_on_user_id"
-  end
-
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.text "overview"
@@ -56,14 +48,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_16_174542) do
     t.index ["list_id"], name: "index_reviews_on_list_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   add_foreign_key "bookmarks", "lists"
   add_foreign_key "bookmarks", "movies"
-  add_foreign_key "movie_lists", "users"
   add_foreign_key "reviews", "lists"
 end
